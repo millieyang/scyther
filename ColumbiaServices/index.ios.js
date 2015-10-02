e081658c978ab6bc44a06bacd5ccd96148bd5efd
Millie Yang <my2440@columbia.edu>
2015-10-02 16:55:51 -0400

    Updated view frame, worked on TouchableHighlight

--- ColumbiaServices/index.ios.js
+++ ColumbiaServices/index.ios.js
@@ -17,96 +17,30 @@ var {
   View,
 } = React;
 
-var REQUEST_URL = 'https://raw.githubusercontent.com/facebook/react-native/master/docs/MoviesExample.json';
+var SearchPage = require('./SearchPage');
 
-var ColumbiaServices = React.createClass({
-  getInitialState: function() {
-    return {
-      dataSource: new ListView.DataSource({
-        rowHasChanged: (row1, row2) => row1 !== row2,
-      }),
-      loaded: false,
-    };
+var styles = React.StyleSheet.create({
+  text: {
+    color: 'black',
+    backgroundColor: 'white',
+    fontSize: 30,
+    margin: 80
   },
-  componentDidMount: function(){
-    this.fetchData();
-  },
-
-  fetchData: function() {
-    fetch(REQUEST_URL)
-      .then((response) => response.json())
-      .then((responseData) => {
-        this.setState({
-          dataSource: this.state.dataSource.cloneWithRows(responseData.movies),
-          loaded: true,
-        });
-      })
-      .done();
-  },
-
-  render: function() {
-    if(!this.state.loaded){
-      return this.renderLoadingView();
-    }
-    return(
-      <ListView
-        dataSource = {this.state.dataSource}
-        renderRow = {this.renderMovie}
-        style = {styles.listview}/>
-    );
-},
-
-  renderLoadingView: function() {
-    return (
-      <View style={styles.container}>
-      <Text>
-      Loading movies...
-      </Text>
-      </View>
-      );
-  },
-
-  renderMovie: function(movie){
-    return (
-      <View style = {styles.container}>
-      <Image 
-      source = {{uri: movie.posters.thumbnail}}
-      style = {styles.thumbnail} />
-      <View style = {styles.rightContainer}>
-      <Text style = {styles.title}>{movie.title}</Text>
-      <Text style = {styles.year}>{movie.year}</Text>
-      </View>
-      </View>
-      );
-  },
+  container: {
+    flex: 1
+  }
 });
-  var styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      flexDirection: 'row',
-      justifyContent: 'center',
-      alignItems: 'center',
-      backgroundColor: '#F5FCFF',
-    },
-    rightContainer:{
-      flex:1,
-    },
-    title:{
-      fontSize: 20,
-      marginBottom: 8,
-      textAlign: 'center',
-    },
-    year:{
-      textAlign: 'center',
-    },
-    thumbnail: {
-      width: 53,
-      height: 81,
-    },
-    listView: {
-    paddingTop: 20,
-    backgroundColor: '#F5FCFF',
-    },
-  });
 
-  AppRegistry.registerComponent('ColumbiaServices', () => ColumbiaServices);
+class ColumbiaServices extends React.Component {
+  render() {
+    return (
+      <React.NavigatorIOS
+        style={styles.container}
+        initialRoute={{
+          title: 'Columbia Services',
+          component: SearchPage,
+        }}/>
+    );
+  }
+}
+  React.AppRegistry.registerComponent('ColumbiaServices', function(){return ColumbiaServices});
