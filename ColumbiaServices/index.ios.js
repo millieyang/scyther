1e00d09f7a37222d03350bca78afedaa06adea96
Millie Yang <my2440@columbia.edu>
2015-10-02 15:30:59 -0400

    Tested with the Rotten Tomatotes API

--- ColumbiaServices/index.ios.js
+++ ColumbiaServices/index.ios.js
@@ -2,52 +2,111 @@
  * Sample React Native App
  * https://github.com/facebook/react-native
  */
-'use strict';
+ 'use strict';
 
-var React = require('react-native');
+ var React = require('react-native');
+// var MOCKED_MOVIES_DATA = [
+//   {title: 'Title', year: '2015', posters: {thumbnail: 'http://i.imgur.com/UePbdph.jpg'}},
+// ];
 var {
   AppRegistry,
+  Image,
+  ListView,
   StyleSheet,
   Text,
   View,
 } = React;
 
+var REQUEST_URL = 'https://raw.githubusercontent.com/facebook/react-native/master/docs/MoviesExample.json';
+
 var ColumbiaServices = React.createClass({
+  getInitialState: function() {
+    return {
+      dataSource: new ListView.DataSource({
+        rowHasChanged: (row1, row2) => row1 !== row2,
+      }),
+      loaded: false,
+    };
+  },
+  componentDidMount: function(){
+    this.fetchData();
+  },
+
+  fetchData: function() {
+    fetch(REQUEST_URL)
+      .then((response) => response.json())
+      .then((responseData) => {
+        this.setState({
+          dataSource: this.state.dataSource.cloneWithRows(responseData.movies),
+          loaded: true,
+        });
+      })
+      .done();
+  },
+
   render: function() {
+    if(!this.state.loaded){
+      return this.renderLoadingView();
+    }
+    return(
+      <ListView
+        dataSource = {this.state.dataSource}
+        renderRow = {this.renderMovie}
+        style = {styles.listview}/>
+    );
+},
+
+  renderLoadingView: function() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
+      <Text>
+      Loading movies...
+      </Text>
       </View>
-    );
-  }
-});
+      );
+  },
 
-var styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+  renderMovie: function(movie){
+    return (
+      <View style = {styles.container}>
+      <Image 
+      source = {{uri: movie.posters.thumbnail}}
+      style = {styles.thumbnail} />
+      <View style = {styles.rightContainer}>
+      <Text style = {styles.title}>{movie.title}</Text>
+      <Text style = {styles.year}>{movie.year}</Text>
+      </View>
+      </View>
+      );
+  },
+});
+  var styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      flexDirection: 'row',
+      justifyContent: 'center',
+      alignItems: 'center',
+      backgroundColor: '#F5FCFF',
+    },
+    rightContainer:{
+      flex:1,
+    },
+    title:{
+      fontSize: 20,
+      marginBottom: 8,
+      textAlign: 'center',
+    },
+    year:{
+      textAlign: 'center',
+    },
+    thumbnail: {
+      width: 53,
+      height: 81,
+    },
+    listView: {
+    paddingTop: 20,
     backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
+    },
+  });
 
-AppRegistry.registerComponent('ColumbiaServices', () => ColumbiaServices);
+  AppRegistry.registerComponent('ColumbiaServices', () => ColumbiaServices);
