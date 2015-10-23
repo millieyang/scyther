0c7b1e4b029d87faacba499aed51930c9c680940
Alex Yu <alexyu@Alexs-Mac-mini.local>
2015-10-23 18:22:27 -0400

    Reorganizing code; resetting ColumbiaServices folder

--- ColumbiaServices/index.ios.js
+++ ColumbiaServices/index.ios.js
@@ -1,36 +1,55 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- */
- 'use strict';
+'use strict';
 
- var React = require('react-native');
- var SearchPage = require('./SearchPage');
-// var MOCKED_MOVIES_DATA = [
-//   {title: 'Title', year: '2015', posters: {thumbnail: 'http://i.imgur.com/UePbdph.jpg'}},
-// ];
+var React = require('react-native');
+var Parse = require('parse/react-native');
+var ParseReact = require('parse-react/react-native');
+Parse.initialize(
+  "ZMvu44Kw2YdxPJRXQXikrgXZxbaQIGbZO9AdYuaB",
+  "PLdqLMI8dtZEpCM4VLFGK7DGC0Wf8SNle76xG1X5"
+);
 var {
   AppRegistry,
   StyleSheet,
+  Text,
+  View,
 } = React;
 
-var SearchPage = require('./SearchPage');
+var ColumbiaServices = React.createClass({
+  render: function() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>
+          Welcome to React Native!
+        </Text>
+        <Text style={styles.instructions}>
+          To get started, edit index.ios.js
+        </Text>
+        <Text style={styles.instructions}>
+          Press Cmd+R to reload,{'\n'}
+          Cmd+D or shake for dev menu
+        </Text>
+      </View>
+    );
+  }
+});
 
 var styles = StyleSheet.create({
   container: {
     flex: 1,
-  }
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
 });
 
-var ColumbiaServiesApp = React.createClass({
-  render: function() {
-    return (
-      <React.NavigatorIOS
-        style={styles.container}
-        initialRoute={{
-          title: 'Property Finder',
-          component: SearchPage,
-        }}/>
-    );
-  }
-});
+AppRegistry.registerComponent('ColumbiaServices', () => ColumbiaServices);
