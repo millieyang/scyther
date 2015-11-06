856c757cb7c7691360ceaabdfca781cf8552318c
Alex Yu <afy2103@columbia.edu>
2015-11-06 17:02:43 -0500

    Changed index to redirect to LoginPage component, and created LoginPage component, which will query Parse for valid username/password, and will only redirect to further components if one chooses to signup or the login is valid. 	new file:   ColumbiaServices/LoginPage.js 	modified:   ColumbiaServices/index.ios.js

--- ColumbiaServices/index.ios.js
+++ ColumbiaServices/index.ios.js
@@ -17,18 +17,12 @@ var {
 var ColumbiaServices = React.createClass({
   render: function() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-      </View>
+      <React.NavigatorIOS
+        style={styles.container}
+        initialRoute={{
+          title: 'Login',
+          component: LoginPage,
+        }}/>
     );
   }
 });
@@ -39,16 +33,6 @@ var styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
   },
 });
 
